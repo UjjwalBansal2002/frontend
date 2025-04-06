9c3cb349c44be0b01ed085004f362108390a1c96
Ujjwal <bansalujjwal2002@gmail.com>
2025-04-06 23:30:47 +0530

    ads

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -13,7 +13,7 @@ export default function AdminLogin() {
     setErrorMsg("");
 
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/admin`, {
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/admin/login`, {
         email,
         password,
       });
